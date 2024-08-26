7e2b9757fb1be021d0954dc437cfd7478b34313a
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-27 02:06:30 +0700

    Lecture02

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -1,23 +1,10 @@
 x = 20
 y = 3
 
-#Output: x + y = 23
-print('x + y =',x+y)
-
-#Output: x- y = 17
-print('x - y =',x-y)
-
-#Output: x * y = 60
-print('x * y =',x*y)
-
-#Output: x / y = 6.666666666666667
-print('x / y =',x/y)
-
-#Output: x % y = 2 หารแล้วเอาเศษมาแสดง
-print('x % y =',x%y)
-
-#Output: x // y = 6 เป็นการหารแบบปัดเศษลง
-print('x // y =',x//y)
-
-#Output: x ** y = 8000 ยกกำลัง
-print('x ** y =',x**y)
+print('x + y =',x+y)#บวก
+print('x - y =',x-y)#ลบ
+print('x * y =',x*y)#คูณ
+print('x / y =',x/y)#หาร
+print('x % y =',x%y)#หารแล้วเอาเศษมาแสดง
+print('x // y =',x//y)#ป็นการหารแบบปัดเศษลง
+print('x ** y =',x**y)#ยกกำลัง
--- example004.py
+++ example004.py
@@ -1,10 +1,10 @@
-boys = int(input('How many boys in classroom:'))
-girls = int(input('How many girls in classroom:'))
+boys = int(input('How many boys in classroom:'))#ใส่จำนวนนักเรียนชาย
+girls = int(input('How many girls in classroom:'))#ใส่จำนวนนักเรียนหญิง
 
-total_student = boys+girls
+total_student = boys+girls#หาจำนวนของนักเรียนทั้งห้อง
 
-percentage_boys = (boys/total_student)*100
+percentage_boys = (boys/total_student)*100#หาเปอร์เซนของนักเรียนชายต่อนักเรียนทั้งห้อง
 print(f"The percentage is: %", percentage_boys)
 
-percentage_girls = (girls/total_student)*100
+percentage_girls = (girls/total_student)*100#หาเปอร์เซนของนักเรียนหญิงต่อนักเรียนทั้งห้อง
 print(f"The percentage is: %", percentage_girls)
