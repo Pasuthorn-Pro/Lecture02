2f2bdde28bd8aeee55fbfec42d9b6bbd1ea88a5c
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-27 15:51:13 +0700

    Lecture02

--- example002.py
+++ example002.py
@@ -1,4 +1,5 @@
+#คำนวณ BMI
 weight = float(input('Enter your weight in Kilograms: '))
 height = float(input('Enter your height in meters '))
-bmi = weight/(height*height)
-print('Your BMI is: ', format(bmi, '5.2f'))
+bmi = weight/(height*height)#สูตรคำนวณหา BMI
+print('Your BMI is: ', format(bmi, '.2f'))
